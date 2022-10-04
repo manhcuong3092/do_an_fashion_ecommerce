52e967acfc61375d45ce54b483aeb6719f604255
manhcuong3092 <cuongdm3092@gmail.com>
2022-10-04 21:21:11 +0700

    Create forget password API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
+const sendEmail = require('../utils/sendEmail');
 
 //Register a user => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -54,4 +55,41 @@ exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
     success: true,
     message: 'Logged out'
   });
-})
+});
+
+
+// Forgot password => /api/v1/password/forgot
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new ErrorHandler(`Không tìm thấy user của email ${req.body.email}`, 404));
+  }
+
+  //Get reset token
+  const resetToken = user.getResetPasswordToken();
+  await user.save({ validateBeforeSave: false });
+
+  //Create reset password url
+  const resetUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
+
+  const message = `Đường link thay đổi mật khẩu của bạn ở dưới đây: \n\n${resetUrl}\n\n
+    Nếu không phải yêu cầu đổi mật khẩu, bỏ qua nó`;
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Email khôi phục mật khẩu',
+      message
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email đã gửi đến ${user.email}`
+    })
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
