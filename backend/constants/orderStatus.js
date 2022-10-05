66aae38f8042bf4dc643f4980dde0bb0e3a1df06
manhcuong3092 <cuongdm3092@gmail.com>
2022-10-05 14:23:58 +0700

    Add admin handle order APIs

---
+++ backend/constants/orderStatus.js
@@ -0,0 +1,4 @@
+exports.SUCCEEDED = 'Giao hàng thành công';
+exports.CANCELLED = 'Đã hủy';
+exports.DELIVERING = 'Đang giao hàng';
+exports.PENDING = 'Đang xử lý';
--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,7 @@ const Order = require('../models/order');
 const Product = require('../models/product');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const { SUCCEEDED, CANCELLED, DELIVERING, PENDING } = require('../constants/orderStatus');
 
 //create new order => /api/v1/order/new
 exports.newOrder = catchAsyncErrors(async (req, res, next) => {
@@ -43,7 +44,7 @@ exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
     .populate('orderItems.size', 'name')
     .populate('orderItems.color', 'name');
   if (!order) {
-    return next(new ErrorHandler('No order found with this ID', 404));
+    return next(new ErrorHandler('Không tìm thấy đơn hàng', 404));
   }
 
   res.status(200).json({
@@ -59,5 +60,83 @@ exports.myOrders = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     orders
+  })
+})
+
+// get all orders => /api/v1/admin/orders - admin
+exports.allOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+  orders.forEach(order => {
+    totalAmount += order.totalPrice
+  });
+
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders
+  })
+})
+
+// update/process orders => /api/v1/admin/order/:id - admin
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  
+  if (order.orderStatus === SUCCEEDED) {
+    return next(new ErrorHandler('Bạn đã giao hàng rồi', 400));
+  }
+
+  if (order.orderStatus === CANCELLED) {
+    return next(new ErrorHandler('Đơn hàng đã bị hủy rồi', 400));
+  }
+
+  if (req.body.status === DELIVERING) {
+    order.orderItems.forEach(async item => {
+      await updateStock(item.product, item.size, item.color, item.quantity);
+    });
+  
+    order.orderStatus = req.body.status;
+    order.deliveredAt = Date.now();
+  } else if (req.body.status === CANCELLED) {
+    order.orderStatus = req.body.status;
+  } else if (req.body.status === SUCCEEDED) {
+    if (order.orderStatus === PENDING) {
+      return next(new ErrorHandler('Đơn hàng chưa được giao', 400));
+    }
+    order.orderStatus = req.body.status;
+  }
+  
+  await order.save()
+  
+  res.status(200).json({
+    success: true,
+    order
+  })
+})
+
+
+async function updateStock(id, size, color, quantity) {
+  const product = await Product.findById(id);
+  product.stock.forEach((item, index) => {
+    if (item.size.toString() === size.toString() && item.color.toString() === color.toString()) {
+      product.stock[index].quantity = product.stock[index].quantity - quantity;
+    }
+  })
+  await product.save({ validateBeforeSave: false })
+}
+
+
+// delete order => /api/v1/admin/order/:id
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(new ErrorHandler('Không tìm thấy đơn hàng', 404));
+  }
+
+  await order.remove();
+
+  res.status(200).json({
+    success: true
   })
 })
