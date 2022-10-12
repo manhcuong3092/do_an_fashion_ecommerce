2f184348a3060fd2b89d0c60c658a5c66675e5a5
manhcuong3092 <cuongdm3092@gmail.com>
2022-10-12 16:01:17 +0700

    Make style admin

--- frontend/src/layouts/Admin/TopNav/index.js
+++ frontend/src/layouts/Admin/TopNav/index.js
@@ -1,28 +1,36 @@
 import React from 'react'
+import logo from '../../../assets/img/logocrop.png'
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import defaultAvatar from '../../../assets/img/default_avatar.jpg'
+import { Link } from 'react-router-dom';
 
 const TopNav = () => {
+  const handleExpandSidebar = (e) => {
+    e.preventDefault();
+    document.body.classList.toggle('sb-sidenav-toggled');
+    localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+  }
+
   return (
-    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-      <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
-      <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-      <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-        <div class="input-group">
-          <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-          <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+    <Navbar className="sb-topnav navbar-expand navbar-dark">
+      <Navbar.Brand className="ps-3 mr-5" href="index.html"><img src={logo} width={40} height={40} alt="logo"/> <span className="ml-5">Trang Quản Trị</span></Navbar.Brand>
+      <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" onClick={handleExpandSidebar}><i className="fas fa-bars"></i></button>
+      <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+        <div className="input-group">
+          <input className="form-control" type="text" placeholder="Tìm kiếm..." aria-label="Tìm kiếm..." aria-describedby="btnNavbarSearch" />
+          <button className="btn btn-primary" id="btnNavbarSearch" type="button"><i className="fas fa-search"></i></button>
         </div>
       </form>
-      <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#!">Settings</a></li>
-            <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-            <li><hr class="dropdown-divider" /></li>
-            <li><a class="dropdown-item" href="#!">Logout</a></li>
-          </ul>
-        </li>
+      <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4" id='nav-ul'>
+        <NavDropdown id="admin-nav" align="end" title={(<img src={defaultAvatar} width={40} height={40} alt='avatar' className="rounded" />)}>
+          <NavDropdown.Item as={Link} to="/">Cài đặt</NavDropdown.Item>
+          <NavDropdown.Item href="#!">Hoạt động</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#!">Đăng xuất</NavDropdown.Item>
+        </NavDropdown>
       </ul>
-    </nav>
+    </Navbar>
   )
 }
 
