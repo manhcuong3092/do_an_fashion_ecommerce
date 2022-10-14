f3636558f9f13c7d4143d685ccb9f02b49179b49
manhcuong3092 <cuongdm3092@gmail.com>
2022-10-14 21:49:54 +0700

    Add reply contact API

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,6 +1,7 @@
 const Contact = require('../models/contact');
 const catchAsyncError = require('../middlewares/catchAsyncErrors');
 const ErrorHandler = require('../utils/errorHandler');
+const sendEmail = require('../utils/sendEmail');
 
 
 exports.createContact = catchAsyncError(async (req, res, next) => {
@@ -29,6 +30,32 @@ exports.getContact = catchAsyncError(async (req, res, next) => {
     success: true,
     contact
   });
+}) 
+
+exports.replyContact = catchAsyncError(async (req, res, next) => {
+  const id = req.params.id;
+  const contact = await Contact.findById(id);
+  if (!contact) {
+    return next(new ErrorHandler(`Không tìm thấy contact: ${req.params.id}`, 404));
+  }
+  try {
+    await sendEmail({
+      email: contact.email,
+      subject: 'Phản hồi liên hệ',
+      message: req.body.content
+    });
+
+    contact.status = 'Đã phản hồi';
+    contact.save()
+
+  } catch (error) {
+    console.log(error);
+    return next(new ErrorHandler(`Không gửi được email phản hồi ${req.params.id}`, 400));
+  }
+  res.status(200).json({
+    success: true,
+    message: `Email đã gửi đến ${contact.email}`
+  })
 }) 
 
 exports.deleteContact = catchAsyncError(async (req, res, next) => {
--- backend/routes/contactRoute.js
+++ backend/routes/contactRoute.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllContacts, createContact, getContact, deleteContact } = require('../controllers/contactController');
+const { getAllContacts, createContact, getContact, deleteContact, replyContact } = require('../controllers/contactController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.route('/contact').post(createContact);
 router.route('/admin/contacts').get(isAuthenticatedUser, authorizeRoles('admin'), getAllContacts);
 router.route('/admin/contact/:id')
   .get(isAuthenticatedUser, authorizeRoles('admin'), getContact)
+  .put(isAuthenticatedUser, authorizeRoles('admin'), replyContact)
   .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteContact);
 
 
