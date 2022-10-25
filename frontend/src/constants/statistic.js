4b22b80d9649e2562301f555eb03859fcd2354d4
manhcuong3092 <cuongdm3092@gmail.com>
2022-10-25 15:29:26 +0700

    Add statistic by order

--- frontend/src/constants/statistic.js
+++ frontend/src/constants/statistic.js
@@ -1,3 +1,3 @@
-export const REVENUE_DAY = 'Ngày';
-export const REVENUE_WEEK = 'Tuần';
-export const REVENUE_MONTH = 'Tháng';
+export const TYPE_DAY = 'Ngày';
+export const TYPE_WEEK = 'Tuần';
+export const TYPE_MONTH = 'Tháng';
--- frontend/src/pages/Admin/Statistic/Statistic.js
+++ frontend/src/pages/Admin/Statistic/Statistic.js
@@ -7,16 +7,28 @@ import Metadata from '~/layouts/Metadata';
 import { END_POINT } from '~/config';
 import { Col, NavDropdown, Row } from 'react-bootstrap';
 import OutlineBox from '~/components/OutlineBox';
-import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
-import { REVENUE_DAY, REVENUE_MONTH, REVENUE_WEEK } from '~/constants/statistic';
+import { TYPE_DAY, TYPE_MONTH, TYPE_WEEK } from '~/constants/statistic';
 
 const Statistic = () => {
   const [orders, setOrders] = useState(null);
   const [products, setProducts] = useState(null);
   const [users, setUsers] = useState(null);
-  const [revenue, setRevenue] = useState(REVENUE_DAY);
+  const [revenueView, setRevenueView] = useState(TYPE_DAY);
+  const [orderView, setOrderView] = useState(TYPE_DAY);
 
   useEffect(() => {
     const config = { withCredentials: true };
@@ -35,9 +47,10 @@ const Statistic = () => {
   }, []);
 
   let revenueData = [];
+  let orderData = [];
 
   if (orders && products && users) {
-    if (revenue === REVENUE_DAY) {
+    if (revenueView === TYPE_DAY) {
       let last10Day = new Date(new Date().getTime() - 10 * 24 * 60 * 60 * 1000);
       last10Day.setHours(0, 0, 0, 0);
       for (let i = 0; i < 10; i++) {
@@ -55,20 +68,16 @@ const Statistic = () => {
         });
         last10Day = nextDay;
       }
-    }
-    if (revenue === REVENUE_WEEK) {
+    } else if (revenueView === TYPE_WEEK) {
       let last10Week = new Date(new Date().getTime() - 10 * 24 * 60 * 60 * 7 * 1000);
+      const thislast10Week = last10Week.getDate();
+      const currentlast10Week = last10Week.getDay();
+      last10Week.setDate(thislast10Week - currentlast10Week + 7);
       last10Week.setHours(0, 0, 0, 0);
       for (let i = 0; i < 10; i++) {
         const thisWeek = new Date(last10Week);
         const nextWeek = new Date(last10Week.setDate(last10Week.getDate() + 7));
         const filterData = orders.filter((item) => {
-          if (item.paidAt) {
-            console.log(new Date(item.paidAt));
-            console.log(thisWeek);
-            console.log(nextWeek);
-            console.log(new Date(item.paidAt) >= thisWeek && new Date(item.paidAt) < nextWeek);
-          }
           return item.paidAt && new Date(item.paidAt) >= thisWeek && new Date(item.paidAt) < nextWeek;
         });
         const totalRevenue = filterData.reduce((acc, item) => {
@@ -80,20 +89,14 @@ const Statistic = () => {
         });
         last10Week = nextWeek;
       }
-    }
-    if (revenue === REVENUE_MONTH) {
-      let last12Month = new Date(new Date().getTime() - 12 * 24 * 60 * 60 * 30 * 1000);
-      last12Month.setHours(0, 0, 0, 0);
+    } else if (revenueView === TYPE_MONTH) {
+      let date = new Date(),
+        y = date.getFullYear();
+      let last12Month = new Date(y, 0, 1);
       for (let i = 0; i < 12; i++) {
         const thisMonth = new Date(last12Month);
-        const nextMonth = new Date(last12Month.setDate(last12Month.getDate() + 30));
+        const nextMonth = new Date(last12Month.setMonth(last12Month.getMonth() + 1));
         const filterData = orders.filter((item) => {
-          if (item.paidAt) {
-            console.log(new Date(item.paidAt));
-            console.log(thisMonth);
-            console.log(nextMonth);
-            console.log(new Date(item.paidAt) >= thisMonth && new Date(item.paidAt) < nextMonth);
-          }
           return item.paidAt && new Date(item.paidAt) >= thisMonth && new Date(item.paidAt) < nextMonth;
         });
         const totalRevenue = filterData.reduce((acc, item) => {
@@ -102,6 +105,57 @@ const Statistic = () => {
         revenueData.push({
           name: thisMonth.toLocaleDateString('vi-VN', { month: 'numeric', year: 'numeric' }).substring(),
           totalRevenue,
+        });
+        last12Month = nextMonth;
+      }
+    }
+
+    if (orderView === TYPE_DAY) {
+      let last10Day = new Date(new Date().getTime() - 10 * 24 * 60 * 60 * 1000);
+      last10Day.setHours(0, 0, 0, 0);
+      for (let i = 0; i < 10; i++) {
+        const toDay = new Date(last10Day);
+        const nextDay = new Date(last10Day.setDate(last10Day.getDate() + 1));
+        const filterData = orders.filter((item) => {
+          return new Date(item.createdAt) >= toDay && new Date(item.createdAt) < nextDay;
+        });
+        orderData.push({
+          name: toDay.toLocaleDateString('vi-VN'),
+          orders: filterData.length,
+        });
+        last10Day = nextDay;
+      }
+    } else if (orderView === TYPE_WEEK) {
+      let last10Week = new Date(new Date().getTime() - 10 * 24 * 60 * 60 * 7 * 1000);
+      const thislast10Week = last10Week.getDate();
+      const currentlast10Week = last10Week.getDay();
+      last10Week.setDate(thislast10Week - currentlast10Week + 7);
+      last10Week.setHours(0, 0, 0, 0);
+      for (let i = 0; i < 10; i++) {
+        const thisWeek = new Date(last10Week);
+        const nextWeek = new Date(last10Week.setDate(last10Week.getDate() + 7));
+        const filterData = orders.filter((item) => {
+          return new Date(item.createdAt) >= thisWeek && new Date(item.createdAt) < nextWeek;
+        });
+        orderData.push({
+          name: thisWeek.toLocaleDateString('vi-VN'),
+          orders: filterData.length,
+        });
+        last10Week = nextWeek;
+      }
+    } else if (orderView === TYPE_MONTH) {
+      let date = new Date(),
+        y = date.getFullYear();
+      let last12Month = new Date(y, 0, 1);
+      for (let i = 0; i < 12; i++) {
+        const thisMonth = new Date(last12Month);
+        const nextMonth = new Date(last12Month.setMonth(last12Month.getMonth() + 1));
+        const filterData = orders.filter((item) => {
+          return new Date(item.paidAt) >= thisMonth && new Date(item.paidAt) < nextMonth;
+        });
+        orderData.push({
+          name: thisMonth.toLocaleDateString('vi-VN', { month: 'numeric', year: 'numeric' }).substring(),
+          orders: filterData.length,
         });
         last12Month = nextMonth;
       }
@@ -153,9 +207,27 @@ const Statistic = () => {
     },
   ];
 
+  const data01 = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+    { name: 'Group E', value: 278 },
+    { name: 'Group F', value: 189 },
+  ];
+
+  const data02 = [
+    { name: 'Group A', value: 2400 },
+    { name: 'Group B', value: 4567 },
+    { name: 'Group C', value: 1398 },
+    { name: 'Group D', value: 9800 },
+    { name: 'Group E', value: 3908 },
+    { name: 'Group F', value: 4800 },
+  ];
+
   return (
     <Fragment>
-      <Metadata title={'Danh sách kích cỡ'} />
+      <Metadata title={'Thống kê'} />
       <TopNav />
       <SideNav>
         <main>
@@ -167,15 +239,15 @@ const Statistic = () => {
                   <div style={{ height: '550px' }} className="m-3">
                     <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4" align="end" id="nav-ul">
                       <NavDropdown id="admin-nav" align="end" title="Xem theo">
-                        <NavDropdown.Item onClick={() => setRevenue(REVENUE_DAY)}>Ngày</NavDropdown.Item>
-                        <NavDropdown.Item onClick={() => setRevenue(REVENUE_WEEK)}>Tuần</NavDropdown.Item>
-                        <NavDropdown.Item onClick={() => setRevenue(REVENUE_MONTH)}>Tháng</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => setRevenueView(TYPE_DAY)}>Ngày</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => setRevenueView(TYPE_WEEK)}>Tuần</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => setRevenueView(TYPE_MONTH)}>Tháng</NavDropdown.Item>
                       </NavDropdown>
                     </ul>
                     <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4" align="end" id="nav-ul">
-                      {revenue}
+                      {revenueView}
                     </ul>
-                    <h4>Thống kê doanh thu theo {revenue}</h4>
+                    <h4>Thống kê doanh thu theo {revenueView}</h4>
                     <ResponsiveContainer width="100%" height="80%">
                       <BarChart
                         width={500}
@@ -193,7 +265,7 @@ const Statistic = () => {
                         <YAxis />
                         <Tooltip />
                         <Legend />
-                        <Bar dataKey="totalRevenue" fill="#82ca9d" />
+                        <Bar name="Tổng" dataKey="totalRevenue" fill="#82ca9d" />
                       </BarChart>
                     </ResponsiveContainer>
                   </div>
@@ -205,20 +277,20 @@ const Statistic = () => {
                   <div style={{ height: '550px' }} className="m-3">
                     <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4" align="end" id="nav-ul">
                       <NavDropdown id="admin-nav" align="end" title="Xem theo">
-                        <NavDropdown.Item>Ngày</NavDropdown.Item>
-                        <NavDropdown.Item>Tuần</NavDropdown.Item>
-                        <NavDropdown.Item>Tháng</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => setOrderView(TYPE_DAY)}>Ngày</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => setOrderView(TYPE_WEEK)}>Tuần</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => setOrderView(TYPE_MONTH)}>Tháng</NavDropdown.Item>
                       </NavDropdown>
                     </ul>
                     <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4" align="end" id="nav-ul">
-                      Test
+                      {orderView}
                     </ul>
-                    <h4>Thống kê doanh thu theo ngày</h4>
+                    <h4>Thống kê số lượng đơn hàng theo {orderView}</h4>
                     <ResponsiveContainer width="100%" height="80%">
                       <BarChart
                         width={500}
                         height={300}
-                        data={data}
+                        data={orderData}
                         margin={{
                           top: 5,
                           right: 30,
@@ -231,9 +303,35 @@ const Statistic = () => {
                         <YAxis />
                         <Tooltip />
                         <Legend />
-                        <Bar dataKey="pv" fill="#8884d8" />
-                        <Bar dataKey="uv" fill="#82ca9d" />
+                        <Bar name="Số đơn" dataKey="orders" fill="#8884d8" />
                       </BarChart>
+                    </ResponsiveContainer>
+                  </div>
+                </OutlineBox>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col md={4}>
+                <OutlineBox>
+                  <div style={{ height: '350px' }} className="m-3">
+                    <h5>Thống kê phương thức thanh toán</h5>
+                    <h6>Theo tháng</h6>
+                    <ResponsiveContainer width="100%" height="80%">
+                      <PieChart width={400} height={400}>
+                        <Pie
+                          dataKey="value"
+                          isAnimationActive={false}
+                          data={data01}
+                          cx="50%"
+                          cy="50%"
+                          outerRadius={80}
+                          fill="#8884d8"
+                          label
+                        />
+                        <Tooltip />
+                        <Legend />
+                      </PieChart>
                     </ResponsiveContainer>
                   </div>
                 </OutlineBox>
