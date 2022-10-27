b78fa2ed9b699e41264ff0e6b0d0d4c206d59c74
manhcuong3092 <cuongdm3092@gmail.com>
2022-10-28 00:04:53 +0700

    Update config

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,1 +1,3 @@
-export const END_POINT = 'http://localhost:4000';
+// export const END_POINT = 'http://localhost:4000';
+export const END_POINT = 'https://fashion-ecommerce-backend.herokuapp.com';
+// export const END_POINT = 'http://ec2-3-133-97-69.us-east-2.compute.amazonaws.com';
