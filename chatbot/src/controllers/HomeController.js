c06ab6dea92c12113d9f68da43bfd377d532029e
manhcuong3092 <cuongdm3092@gmail.com>
2022-10-28 17:57:25 +0700

    Update handle event message

--- chatbot/src/controllers/HomeController.js
+++ chatbot/src/controllers/HomeController.js
@@ -1,26 +1,46 @@
 require('dotenv').config();
 
+const request = require('request');
+
 const getHomePage = (req, res) => {
   return res.render('homepage.ejs');
 }
 
 const postWebhook = (req, res) => {
-  let body = req.body;
-  console.log(req.body);
 
-  //Check this is an event from a page subscriptinon
-  if(body.object === 'page') {
-    //Iterates over each entry - there may be multiple if batched
-    body.entry.forEach((entry) => {
-      //Get message entry.messaging is array, but only contain 1 mess, so get index 0
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function (entry) {
+
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+
     });
 
-    //Return a '200 OK' response to all requests
+    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
+
   } else {
-    //Return 404 if event not from a page subscription
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 }
@@ -44,6 +64,53 @@ const getWebhook = (req, res) => {
       res.sendStatus(403);
     }
   }
+}
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
 }
 
 module.exports = {
