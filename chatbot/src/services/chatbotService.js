0f571fa76828a91b38cb04006d41521b7939fe04
manhcuong3092 <cuongdm3092@gmail.com>
2022-10-28 22:44:24 +0700

    Add get username fb

--- chatbot/src/services/chatbotService.js
+++ chatbot/src/services/chatbotService.js
@@ -6,12 +6,34 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 const handleGetStarted = (sender_psid) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const response = { "text": "Ok. Xin chào mừng bạn đến với shop Amando." }
+      const username = await getUsername(sender_psid);
+      const response = { "text": `Ok. Xin chào mừng bạn ${username} đến với shop Amando.` }
       await callSendAPI(sender_psid, response);
       resolve('done');
     } catch (e) {
       reject(e);
     }
+  });
+}
+
+let getUsername = (sender_psid) => {
+  // Send the HTTP request to the Messenger Platform
+  return new Promise((resolve, reject) => {
+    request({
+      "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+      "qs": { "access_token": PAGE_ACCESS_TOKEN },
+      "method": "GET",
+    }, (err, res, body) => {
+      console.log(body);
+      if (!err) {
+        let response = JSON.parse(body);
+        let username = `${response.last_name} ${response.first_name}`;
+        resolve(username);
+      } else {
+        console.error("Unable to send message:" + err);
+        reject(err)
+      }
+    });
   })
 }
 
