88e5fb355137e1092ab7661e9a8bf12d1f81d410
manhcuong3092 <cuongdm3092@gmail.com>
2022-10-29 23:37:27 +0700

    Update product API

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const router = express.Router();
 
-const { 
-  newProduct, getProducts, getSingleProduct, deleteProduct, 
-  updateProduct, getAllProducts, getProductBySlug, createProductReview, 
-  getProductReviews, deleteReview, getLatestProducts, 
+const {
+  newProduct, getProducts, getSingleProduct, deleteProduct,
+  updateProduct, getAllProducts, getProductBySlug, createProductReview,
+  getProductReviews, deleteReview, getLatestProducts,
 } = require('../controllers/productController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.route('/products').get(getProducts);
 router.route('/products/latest').get(getLatestProducts);
-// router.route('/product/:id').get(getSingleProduct)
-router.route('/product/:slug').get(getProductBySlug);
+router.route('/product/:id').get(getSingleProduct)
+router.route('/product/slug/:slug').get(getProductBySlug);
 router.route('/admin/products').get(isAuthenticatedUser, authorizeRoles('admin'), getAllProducts);
 router.route('/admin/product').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
 router.route('/admin/product/:id')
