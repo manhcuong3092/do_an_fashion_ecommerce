aa4492d75f0ce1310d56a979f244f71dac27a800
manhcuong3092 <cuongdm3092@gmail.com>
2022-12-05 22:31:42 +0700

    Change call API endpoint

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,3 +1,2 @@
-export const END_POINT = 'http://13.229.70.185';
-// export const END_POINT = 'https://fashion-ecommerce-backend.herokuapp.com';
-// export const END_POINT = 'http://ec2-3-133-97-69.us-east-2.compute.amazonaws.com';
+export const END_POINT = 'https://amando.store';
+// export const END_POINT = 'https://localhost';
