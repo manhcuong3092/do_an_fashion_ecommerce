b8f3db66f0111d5f4c87edc0d07892121bdab9ba
manhcuong3092 <cuongdm3092@gmail.com>
2022-12-21 14:39:01 +0700

    update statistic API

--- backend/controllers/statisticController.js
+++ backend/controllers/statisticController.js
@@ -5,29 +5,43 @@ const User = require('../models/user')
 
 exports.getStatistic = catchAsyncErrors(async (req, res, next) => {
   let users, products, orders;
-  const promise1 = Order.find().populate('user', 'name email')
-    .populate({
-      path: 'orderItems.product',
-      model: 'Product',
-      populate: {
-        path: 'category',
-        model: 'Category'
+  const promise1 = Order.aggregate([
+    {
+      $lookup: { from: 'orderitems', localField: '_id', foreignField: 'order', as: 'orderItems' }
+    },
+    { $unwind: "$orderItems" },
+    { $lookup: { from: 'productitems', localField: 'orderItems.productItem', foreignField: '_id', as: 'orderItems.productItem' } },
+    { $unwind: "$orderItems.productItem" },
+    { $lookup: { from: 'products', localField: 'orderItems.productItem.product', foreignField: '_id', as: 'orderItems.productItem.product' } },
+    { $unwind: "$orderItems.productItem.product" },
+    { $lookup: { from: 'categories', localField: 'orderItems.productItem.product.category', foreignField: '_id', as: 'orderItems.productItem.product.category' } },
+    { $unwind: "$orderItems.productItem.product.category" },
+    {
+      $group: {
+        _id: "$_id",
+        orderItems: { $push: "$orderItems" },
+        shippingInfo: { $first: "$shippingInfo" },
+        user: { $first: "$user" },
+        paymentType: { $first: "$paymentType" },
+        paymentStatus: { $first: "$paymentStatus" },
+        paidAt: { $first: "$paidAt" },
+        itemsPrice: { $first: "$itemsPrice" },
+        shippingPrice: { $first: "$shippingPrice" },
+        totalPrice: { $first: "$totalPrice" },
+        orderStatus: { $first: "$orderStatus" },
+        createdAt: { $first: "$createdAt" },
+        deliveredAt: { $first: "$deliveredAt" },
       }
-    })
-    .populate('orderItems.size', 'name')
-    .populate('orderItems.color', 'name');
-  const promise2 = Product.find().populate('category')
-    .populate('sizes')
-    .populate('colors')
-    .populate('stock.size')
-    .populate('stock.color');
+    }
+  ]);
+  const promise2 = Product.find().populate('category');
   const promise3 = User.find();
   await Promise.all([promise1, promise2, promise3]).then((value) => {
     orders = value[0];
     products = value[1];
     users = value[2];
   });
-  
+
   res.status(200).json({
     orders,
     products,
